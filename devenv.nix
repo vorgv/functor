a1cda2f208c2b5f277004bdf036810649dcd10fe
Celio Grand <celiogrand@outlook.com>
2024-05-30 00:50:19 +0800

    hello devenv rust

---
+++ devenv.nix
@@ -0,0 +1,17 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  languages.rust = {
+    enable = true;
+    channel = "nightly";
+    components = [ "rustc" "cargo" "clippy" "rustfmt" "rust-analyzer" ];
+  };
+
+  packages = with pkgs; [
+    openssl
+  ];
+
+  services.cassandra = {
+    enable = true;
+  };
+}
